b0c2d59d6ffb7a120bb4c4df21f779559e4dfe42
TheMS128 <onisimchuk.bogdan@gmail.com>
2025-05-15 15:27:53 +0300

    Ваше сообщение о коммите

--- src/main/java/SiteTest/Pages/HomePage.java
+++ src/main/java/SiteTest/Pages/HomePage.java
@@ -1,6 +1,7 @@
 package SiteTest.Pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -10,15 +11,45 @@ import java.util.List;
 public class HomePage {
     private WebDriver driver;
 
+    protected void findElementWithInput(String searchRequest) {
+        WebElement input = driver.findElement(By.xpath("//input[@placeholder=\"Search\"]"));
+        input.sendKeys(searchRequest, Keys.ENTER);
+    }
+
     public HomePage(WebDriver driver) {
         this.driver = driver;
     }
 
-    public String getSiteTitle() {
+    public String getPageTitle() {
         return driver.getTitle();
     }
 
-    public List<WebElement> getMainSliderWrappers() {
-        return driver.findElements(By.xpath("//div[@class=\"swiper-wrapper\"]"));
+    public String getSiteTitle() {
+        return driver.findElement(By.xpath("//h1/a")).getText();
+    }
+
+    public List<WebElement> getMenuElements() {
+        return driver.findElements(By.xpath("//ul[@class=\"nav navbar-nav\"]/li/a"));
+    }
+
+    public String getCartText() {
+        return driver.findElement(By.xpath("//div[@class=\"col-sm-3\"]//span[@id=\"cart-total\"]"))
+                .getText();
+    }
+
+    public List<WebElement> getSearchPanel() {
+        return driver.findElements(By.xpath("//div[@class=\"col-sm-5\"]"));
+    }
+
+    public List<WebElement> getElementsFromSearchPage(String searchRequest) {
+        findElementWithInput(searchRequest);
+        List<WebElement> elements = List.of();
+        try {
+            elements = driver.findElements(By
+                    .xpath("//div[@class=\"row\"][3]//div[@class=\"product-thumb\"]"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return elements;
     }
 }
--- src/main/java/SiteTest/Test.java
+++ src/main/java/SiteTest/Test.java
@@ -4,14 +4,18 @@ import SiteTest.Pages.HomePage;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class Test {
     ChromeDriver driver;
     HomePage homePage;
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
     @Before
     public void setUp() {
@@ -32,16 +36,94 @@ public class Test {
     }
 
     @org.junit.Test
+    public void testPageTitle() {
+        String expectedPageTitle = "Your Store";
+        String actualPageTitle = homePage.getPageTitle();
+        Assert.assertEquals(expectedPageTitle, actualPageTitle);
+    }
+
+    @org.junit.Test
     public void testSiteTitle() {
-        String expectedSiteTitle = "Your Store";
+        String expectedSiteTitle = "Qafox.com";
         String actualSiteTitle = homePage.getSiteTitle();
         Assert.assertEquals(expectedSiteTitle, actualSiteTitle);
     }
 
     @org.junit.Test
-    public void testShouldHaveTwoMainSliderWrappers() {
-        int expectedNumberElements = 2;
-        int actualNumberElements = homePage.getMainSliderWrappers().size();
+    public void testHaveCartText() {
+        String expectedString = "0 item(s) - $0.00";
+        String actualString = homePage.getCartText();
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @org.junit.Test
+    public void testHave8ElementsInMenu() {
+        int expectedNumberElements = 8;
+        int actualNumberElements = homePage.getMenuElements().size();
         Assert.assertEquals(expectedNumberElements, actualNumberElements);
     }
+
+    @org.junit.Test
+    public void testHaveSearchPanel() {
+        int expectedSearchElements = 1;
+        int actualSearchElements = homePage.getSearchPanel().size();
+        Assert.assertEquals(expectedSearchElements, actualSearchElements);
+    }
+
+    @org.junit.Test
+    public void testSearchPanelWithMacbookValue() {
+        int expectedProductElements = 3;
+        String searchRequest = "Macbook";
+        int actualProductElements = homePage.getElementsFromSearchPage(searchRequest).size();
+        Assert.assertEquals(expectedProductElements, actualProductElements);
+    }
+
+    @org.junit.Test
+    public void testSearchPanelWithIphoneValue() {
+        int expectedProductElements = 1;
+        String searchRequest = "iPhone";
+        int actualProductElements = homePage.getElementsFromSearchPage(searchRequest).size();
+        Assert.assertEquals(expectedProductElements, actualProductElements);
+    }
+
+    @org.junit.Test
+    public void testSearchPanelWithAppleCinema30Value() {
+        int expectedProductElements = 1;
+        String searchRequest = "Apple Cinema 30\"";
+        int actualProductElements = homePage.getElementsFromSearchPage(searchRequest).size();
+        Assert.assertEquals(expectedProductElements, actualProductElements);
+    }
+
+    @org.junit.Test
+    public void testSearchPanelWithCanonEos5DValue() {
+        int expectedProductElements = 1;
+        String searchRequest = "Canon EOS 5D";
+        int actualProductElements = homePage.getElementsFromSearchPage(searchRequest).size();
+        Assert.assertEquals(expectedProductElements, actualProductElements);
+    }
+
+    @org.junit.Test
+    public void testSearchPanelWithOneSpaceValue() {
+        int expectedProductElements = 19;
+        String searchRequest = " ";
+        int actualProductElements = homePage.getElementsFromSearchPage(searchRequest).size();
+        Assert.assertEquals(expectedProductElements, actualProductElements);
+    }
+
+    @org.junit.Test
+    public void testSearchPanelWithEmptyValue() {
+        int expectedProductElements = 0;
+        String searchRequest = "";
+        int actualProductElements = homePage.getElementsFromSearchPage(searchRequest).size();
+        Assert.assertEquals(expectedProductElements, actualProductElements);
+    }
+
+//    @org.junit.Test
+//    public void testSearchPanelWithScriptValue() {
+//        int expectedProductElements = 0;
+//        String searchRequest = "<script>alert(\"Ready for XSS injection\")</script>";
+//        int actualProductElements = homePage.getElementsFromSearchPage(searchRequest).size();
+//        Alert modalWindowXSS = wait.until(ExpectedConditions.not(ExpectedConditions.alertIsPresent()));
+//        Assert.assertEquals(expectedProductElements, actualProductElements);
+//    }
 }
